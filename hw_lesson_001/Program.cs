c2f79fa3892988d264ab08511589baa114f7e212
FisayAA <fisay@fisay.ru>
2023-02-03 17:57:00 +0300

    solution task 2 of lesson 1

---
+++ hw_lesson_001/Program.cs
@@ -0,0 +1,9 @@
+﻿//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+//Решение:
+Console.Clear();
+Console.Write("Please enter the first number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Please enter the second number: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b) Console.WriteLine($"larger umber -> {a}, smaller number -> {b}");
+else Console.WriteLine($"larger umber -> {b}, smaller number -> {a}");
